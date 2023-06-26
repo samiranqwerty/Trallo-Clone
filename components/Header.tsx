c9742f5a0200b2bba96e9239a37923df126e937e
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-06-26 21:43:19 +0530

    added yarn add @heroicons/react and used in Header secction

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
     <header>
-      <Image src="https://res.cloudinary.com/studiovity/image/upload/v1687753565/Trello%20clone/Trello-logo-blue.svg_kkvxtf.png"
-      alt='Trello Logo'
-      width={300}
-      height={100}
-      className='w-44 md:w-56 pb-10 md:pb-0 object-contain'
-
+      <Image
+        src="https://res.cloudinary.com/studiovity/image/upload/v1687753565/Trello%20clone/Trello-logo-blue.svg_kkvxtf.png"
+        alt="Trello Logo"
+        width={300}
+        height={100}
+        className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
       />
-    </header>
-  )
-}
 
-export default Header
+      {/* Search Box */}
+      <form
+        className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial"
+        action=""
+      >
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-1 outline-none"
+        />
+        <button hidden>Search</button>
+      </form>
+      {/* Avatar */}
+    </header>
+  );
+};
+
+export default Header;
