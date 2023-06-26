73678a5b43c3570a39773c1c25cf06cefe1ab61d
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-06-26 23:17:04 +0530

    added yarn add react-beautiful-dnd

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Board from '@/components/Board'
-import Header from '@/components/Header'
+import Board from "@/components/Board";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
-      <Header/>
-
-      {/* Board */}
-     <Board/>
+      <Header />
+      <Board />
     </main>
-  )
+  );
 }
