3fe03674dcf1edb2ebd9d4b7cae25f746f601a4e
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-06-26 22:03:49 +0530

    added core-js-pure in json dependencies

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,41 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
 
 const Header = () => {
   return (
     <header>
-      <Image
-        src="https://res.cloudinary.com/studiovity/image/upload/v1687753565/Trello%20clone/Trello-logo-blue.svg_kkvxtf.png"
-        alt="Trello Logo"
-        width={300}
-        height={100}
-        className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
-      />
-
-      {/* Search Box */}
-      <form
-        className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial"
-        action=""
-      >
-        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="flex-1 outline-none"
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <Image
+          src="https://res.cloudinary.com/studiovity/image/upload/v1687753565/Trello%20clone/Trello-logo-blue.svg_kkvxtf.png"
+          alt="Trello Logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
-        <button hidden>Search</button>
-      </form>
-      {/* Avatar */}
+        <div className="flex">
+          {/* Search Box */}
+          <form
+            className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial"
+            action=""
+          >
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+          {/* Avatar */}
+          <Avatar name="Sonny Sangha"/>
+        </div>
+      </div>
     </header>
   );
 };
